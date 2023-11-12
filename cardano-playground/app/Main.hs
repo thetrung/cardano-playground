0547a8352fea7a9c688d780fab4f40c84ea11a18
thetrung <deulamco@gmail.com>
2023-11-12 18:58:58 +0700

    cleanup & add cardano.binary

--- cardano-playground/app/Main.hs
+++ cardano-playground/app/Main.hs
@@ -1,54 +1,21 @@
 module Main where
-
-import qualified MyLib (someFunc)
-import Crypto.Hash.MerkleTree
-import Data.ByteString as BS
-import Data.Char (ord)
-import qualified Data.ByteString.Char8 as B
-import Data.ByteString.Base16 (encode)
-import Debug.Trace
-
-stringToBytes :: String -> B.ByteString
-stringToBytes = B.pack
-
--- byteStringToHex :: B.ByteString -> String
--- byteStringToHex bs = unpack (encode bs)
-
-mkTreeRoot :: [B.ByteString] -> ByteString
-mkTreeRoot x = do 
-  let mTree = mkMerkleTree x
-  let hashRoot = mtHash mTree
-  traceShow ("hashRoot: " ++ show hashRoot) hashRoot
-
-printMerkleTree :: IO ()
-printMerkleTree = do
-  let datax = [ stringToBytes "aaa" ,  stringToBytes "bbb" , stringToBytes "ccc" , stringToBytes "ddd"]
-  B.putStrLn (mkTreeRoot datax)
+-- Add stuffs in attempt to decode block-data 
+-- | CBOR serialisation
+import           Prelude
+import           Data.ByteString (ByteString)
+import qualified Data.ByteString as ByteString
+import           Cardano.Binary (FromCBOR, ToCBOR)
+import qualified Cardano.Binary as CBOR
 
 loadBlockData :: IO ()
 loadBlockData = do
-  bs <- BS.readFile "cardano-playground/app/data/blocks-21.dat"
-  let byteArray = B.unpack bs
-  B.putStrLn bs
-  Prelude.putStrLn byteArray
+  raw_block <- ByteString.readFile "cardano-playground/app/data/blocks-21.dat"
+  print raw_block
+  --
+  -- still haven't found clues to take use of CBOR from here. 
+  --
 
 main :: IO ()
 main = do
   Prelude.putStrLn "Hello, Haskell!"
-  MyLib.someFunc
-  printMerkleTree
-  -- loadBlockData
-
--- import Data.ByteString.Char8 as B
--- import Data.ByteString.Base16 (encode)
-
--- -- Function to convert ByteString to a hexadecimal string
--- byteStringToHex :: B.ByteString -> String
--- byteStringToHex bs = unpack (encode bs)
-
--- main :: IO ()
--- main = do
---   let inputString = "aaa"  -- Replace this with your ByteString
---   let byteString = B.pack inputString
---   let hexString = byteStringToHex byteString
---   Prelude.putStrLn hexString
+  loadBlockData
